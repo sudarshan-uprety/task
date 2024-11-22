8269053a9a808c84cbb8800ee984c30126851f5e
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-11-22 14:06:51 +0545

    Working on Deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,4 @@ echo "Starting server"
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn --bind 0.0.0.0:80 core.asgi -w 4 -k uvicorn.workers.UvicornWorker
+gunicorn --bind 0.0.0.0:8000 core.asgi -w 4 -k uvicorn.workers.UvicornWorker
