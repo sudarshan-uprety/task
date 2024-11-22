47bd13d26380d2f2ff11c0cda42d15a4adc9dca8
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-11-22 14:52:48 +0545

    Working on Deployment

--- core/settings.py
+++ core/settings.py
@@ -61,8 +61,8 @@ INSTALLED_APPS = INBUILD_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
